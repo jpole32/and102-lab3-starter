b30bcab820e1169d53d58f4bd8b97659cffd377d
jpole329 <jpoleary329@gmail.com>
2024-02-08 12:30:51 -0500

    Done up to Step 2

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksFragment.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksFragment.kt
@@ -1,6 +1,7 @@
 package com.codepath.bestsellerlistapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,16 @@ import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.codepath.bestsellerlistapp.R
+import okhttp3.Headers
 
 // --------------------------------//
 // CHANGE THIS TO BE YOUR API KEY  //
 // --------------------------------//
-private const val API_KEY = "<YOUR-API-KEY-HERE>"
+private const val API_KEY = "I9hdMCHtG61yVOfMbIAhjbjujARQzMGK"
 
 /*
  * The class for the only fragment in the app, which contains the progress bar,
@@ -46,10 +51,15 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
         progressBar.show()
 
         // Create and set up an AsyncHTTPClient() here
-
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        params["api-key"] = API_KEY
         // Using the client, perform the HTTP request
+        client[
+            "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json",
+            params,
+            object : JsonHttpResponseHandler()
 
-        /* Uncomment me once you complete the above sections!
         {
             /*
              * The onSuccess function gets called when
